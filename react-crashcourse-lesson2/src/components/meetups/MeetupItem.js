ad233e22b159beee0a6dc7444a522c618281e34b
Gurbindar Biln <gbiln@biln.ca>
2021-12-29 12:01:05 -0500

    add to favorites functionality added

--- react-crashcourse-lesson2/src/components/meetups/MeetupItem.js
+++ react-crashcourse-lesson2/src/components/meetups/MeetupItem.js
@@ -1,7 +1,29 @@
+import { useContext } from "react";
+
 import classes from "./MeetupItem.module.css";
 import Card from "../ui/Card";
+import FavoritesContext from "../../store/favorites-context";
 
 function MeetupItem(props) {
+  const favoriteCtx = useContext(FavoritesContext);
+  const itemIsFavorite = favoriteCtx.itemIsFavorite(props.id);
+
+  function toggleFavoritesStatusHandler() {
+    if (itemIsFavorite) {
+      favoriteCtx.removeFavorite(props.id);
+    }
+    else {
+      favoriteCtx.addFavorite({
+        id: props.id,
+        title: props.title,
+        description: props.description,
+        image: props.image,
+        address: props.address,
+      })
+    }
+
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -14,7 +36,7 @@ function MeetupItem(props) {
           <p>{props.description}</p>
         </div>
         <div className={classes.actions}>
-          <button>To Favourites</button>
+          <button onClick={toggleFavoritesStatusHandler}>{itemIsFavorite ? 'Remove from Favorites' : "To Favorites"}</button>
         </div>
       </Card>
     </li>
--- react-crashcourse-lesson2/src/index.js
+++ react-crashcourse-lesson2/src/index.js
@@ -4,12 +4,15 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
+import { FavoritesContextProvider } from './store/favorites-context'; 
 
 ReactDOM.render(
   <React.StrictMode>
+    <FavoritesContextProvider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+    </FavoritesContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
